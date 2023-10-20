c410062b85430dbc4ae787623ce42ba0a2567ca2
Nat! <nat@mulle-kybernetik.com>
2023-10-20 19:05:48 +0200

    update mulle-sde project to new version

--- src/MulleHoedown.h
+++ src/MulleHoedown.h
@@ -35,7 +35,7 @@
 #import "NSData+MulleHoedown.h"
 #import "NSString+MulleHoedown.h"
 
-#define MULLE_HOEDOWN_VERSION ((0 << 20) | (3 << 8) | 0)
+#define MULLE_HOEDOWN_VERSION ((0 << 20) | (3 << 8) | 1)
 
 
 #ifdef __has_include
